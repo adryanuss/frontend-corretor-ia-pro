1c0ccb54dcaa5798bcb44ee28b7c9a5e7f0dee52
adryanuss <adryanuss.pr@gmail.com>
2026-02-17 11:55:38 -0300

    feat: add next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-module.exports = nextConfig
+const nextConfig = {
+  async headers() {
+    return [{ source: '/api/:path*', headers: [
+      { key: 'Access-Control-Allow-Origin', value: '*' },
+      { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
+      { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+    ]}];
+  },
+};
+module.exports = nextConfig;
